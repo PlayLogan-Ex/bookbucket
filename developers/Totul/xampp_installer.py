5962e0e127f41673c55de03a571eef1df5767c68
Totul <rytotul@gmail.com>
2018-07-18 23:19:48 +0600

    Updated

--- developers/Totul/xampp_installer.py
+++ developers/Totul/xampp_installer.py
@@ -4,17 +4,20 @@ import sys
 import os
 from argparse import ArgumentParser
 
+# XAMPP Location
 lampp = '/opt/lampp/lampp'
 
+# Installer For XAMPP on Linux
 def install():
     os.system('wget -O xampp.run https://downloadsapachefriends.global.ssl.fastly.net/\
-    xampp-files/7.2.7/xampp-linux-x64-7.2.7-0-installer.run?from_af=true')
+            xampp-files/7.2.7/xampp-linux-x64-7.2.7-0-installer.run?from_af=true')
     os.system('sudo chmod 755 xampp.run')
     os.system('sudo ./xampp.run')
     os.system('rm -rf xampp.run')
 
+# Main Function
 def main():
-    parser = ArgumentParser(description='XAMPP Installer\nAuthor: Totul (fb.com/rytotul).')
+    parser = ArgumentParser(description='XAMPP Installer | Author: Totul (fb.com/rytotul).')
 
     parser.add_argument('-l', '--launch', action='store_true', default=False,
             help='Launches XAMPP')
@@ -34,5 +37,6 @@ def main():
     else:
         parser.print_help()
 
+# Launches main() Function
 if __name__ == '__main__':
     main()
