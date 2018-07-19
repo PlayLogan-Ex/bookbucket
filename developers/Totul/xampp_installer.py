9898f2e7ea6e6e0ea82d73d24ff73ee8b19c3996
Totul <rytotul@gmail.com>
2018-07-19 11:14:51 +0600

    Updated

--- developers/Totul/xampp_installer.py
+++ developers/Totul/xampp_installer.py
@@ -10,7 +10,7 @@ lampp = '/opt/lampp/lampp'
 # Installer For XAMPP on Linux
 def install():
     os.system('wget -O xampp.run https://downloadsapachefriends.global.ssl.fastly.net/\
-            xampp-files/7.2.7/xampp-linux-x64-7.2.7-0-installer.run)
+            xampp-files/7.2.7/xampp-linux-x64-7.2.7-0-installer.run')
     os.system('sudo chmod 755 xampp.run')
     os.system('sudo ./xampp.run')
     os.system('rm -rf xampp.run')
